5507e1a29468d7f25c0b5061e6de39d83b7287ed
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-08 18:51:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-  programmer_hash[:alan_kay][:known_for][1] = "GUI"
+  programmer_hash[:alan_kay] = {
+		:known_for => "Object Orientation", "GUI"
+	}
 	programmer_hash
 end
 
